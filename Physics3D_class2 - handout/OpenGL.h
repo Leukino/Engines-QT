e6efdbdd30297735ce7b30d9f50798552ee8010c
Leukino <miquelqg99@gmail.com>
2021-09-27 18:06:04 +0200

    glew implemented and working fine

---
+++ Physics3D_class2 - handout/OpenGL.h
@@ -0,0 +1,11 @@
+#ifndef __OPEN_GL_H__
+#define __OPEN_GL_H__
+
+#define WIN32_MEAN_AND_LEAN							
+#include <Windows.h>
+#include "Glew/include/GL/glew.h"			
+#include "SDL/include/SDL_opengl.h"
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+#endif // !__OPEN_GL_H__
